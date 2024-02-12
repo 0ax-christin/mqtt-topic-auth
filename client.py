409812e81d25bcc5954a636da1d2a5d7c36dd467
Christin <caa8062@rit.edu>
2024-02-12 14:29:39 +0400

    Added a public key authentication mechanism, which requires that a client sends a public key that is in the servers authorized_keys and is also able to sign the challenge the server sends with the correct private key that corresponds to the public key, if so, the server allows Noise handshake to continue, else, it terminates the connection with the client

--- client.py
+++ client.py
@@ -9,6 +9,8 @@ from dissononce.dh.x25519.x25519 import X25519DH
 from dissononce.hash.blake2s import Blake2sHash
 from dissononce.extras.processing.handshakestate_guarded import GuardedHandshakeState
 
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+
 HOST = "127.0.0.1"
 PORT = 63000 
 
@@ -32,25 +34,53 @@ def main():
 
     client_handshakestate.initialize(XXHandshakePattern(), True, b'', s=client_static)
 
+    with open('public_key.txt', 'rb') as reader:
+        public_bytes = reader.read()
+    with open('private_key.txt', 'rb') as reader:
+        private_bytes = reader.read()
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        # -> e
-        message_buffer = bytearray()
-        client_handshakestate.write_message(b'', message_buffer)
-        s.sendall(message_buffer)
 
-        message_buffer = s.recv(1024)
-        client_handshakestate.read_message(bytes(message_buffer), bytearray())
+        private_key = Ed25519PrivateKey.from_private_bytes(private_bytes)
+        # Send the public key to the server
+        s.sendall(public_bytes)
+        # Receive random challenge bytes to sign
+        random_bits = s.recv(32)
 
-        # -> s,se 
-        message_buffer = bytearray()
-        client_cipherstates = client_handshakestate.write_message(b'', message_buffer)
-        s.sendall(message_buffer)
+        # Sign and send back signed challenge bytes
+        signed_random_bits = private_key.sign(random_bits)
+        s.sendall(signed_random_bits)
 
-        ciphertext = client_cipherstates[0].encrypt_with_ad(b'', b'Hello')
-        print(ciphertext, type(ciphertext), len(ciphertext))
-        s.sendall(ciphertext) 
+        # Receive back the result, which indicates whether authentication passed or failed
+        result = s.recv(1024)
+        print(result)
 
+        if result == b'SUCCESS':
+            # -> e
+            message_buffer = bytearray()
+            client_handshakestate.write_message(b'', message_buffer)
+            # Sends ephemeral public key
+            s.sendall(message_buffer)
+
+            message_buffer = s.recv(1024)
+            # Performs DH between ephemeral keys
+            # Performs DH between ephemeral key of client and static key of server, authenticating server
+            client_handshakestate.read_message(bytes(message_buffer), bytearray())
+
+            # -> s,se 
+            message_buffer = bytearray()
+            client_cipherstates = client_handshakestate.write_message(b'', message_buffer)
+            # Sends public static key of client to server, does the final DH on both sides
+            s.sendall(message_buffer)
+
+            # Save derived cipherstate so it can be used later?
+
+            ciphertext = client_cipherstates[0].encrypt_with_ad(b'', b'Hello')
+            print(ciphertext, type(ciphertext), len(ciphertext))
+            s.sendall(ciphertext) 
+        else:
+            conn.close()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import dissononce, logging, socket
+import dissononce, logging, socket, os, secrets
 
 from dissononce.processing.impl.handshakestate import HandshakeState
 from dissononce.processing.impl.symmetricstate import SymmetricState
@@ -9,9 +9,22 @@ from dissononce.dh.x25519.x25519 import X25519DH
 from dissononce.hash.blake2s import Blake2sHash
 from dissononce.extras.processing.handshakestate_guarded import GuardedHandshakeState
 
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
+from cryptography.exceptions import InvalidSignature
+
 HOST = "127.0.0.1"
 PORT = 63000
-
+'''
+Check that a given public key in byte string exists in authorized_keys
+'''
+def public_key_exists(public_bytes):
+    if os.path.exists("authorized_keys"):
+        with open("authorized_keys", 'rb') as reader:
+            while (current_public_bytes := reader.read(32)):
+                if current_public_bytes == public_bytes:
+                    return True
+                else:
+                    return False
 def main():
     dissononce.logger.setLevel(logging.DEBUG)
 
@@ -36,7 +49,25 @@ def main():
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
-
+            # First, a public key is expected from client
+            public_bytes = conn.recv(32)
+        
+            if public_key_exists(public_bytes=public_bytes):
+                public_key = Ed25519PublicKey.from_public_bytes(public_bytes)
+                # If public key exists in authorized_keys, send random 32 bytes back as a challenge to sign
+                random_bits = secrets.token_bytes(32)
+                conn.sendall(random_bits)
+                signed_random_bits = conn.recv(64)
+                try:
+                    verify_result = public_key.verify(signed_random_bits, random_bits)
+                except InvalidSignature:
+                    conn.sendall(b'INVALID SIGNATURE, AUTH FAILED')
+                    conn.close()
+                    exit()
+                # If no exception has been raised, means that signature was correctly verified
+                if verify_result == None:
+                    conn.sendall(b'SUCCESS')
+            
             # Receiving ephemeral public key from client
             message_buffer = conn.recv(1024)
             
@@ -46,12 +77,16 @@ def main():
             # <- e, ee, s, es
             message_buffer = bytearray() 
             server_handshakestate.write_message(b'', message_buffer) 
-            # Sending message buffer to client
+            # Sending message buffer to client, this will have ephemeral public key of server
+            # Server then performs DH to get shared secret, uses this to send static public key under encryption
             conn.sendall(message_buffer)
 
             # -> s, se
             message_buffer = conn.recv(1024)
-            server_cipherstates = server_handshakestate.read_messae(bytes(message_buffer), bytearray())
+            server_cipherstates = server_handshakestate.read_message(bytes(message_buffer), bytearray())
+            
+            print(server_cipherstates, len(server_cipherstates))
+            print(type(server_cipherstates))
             
             ciphertext = conn.recv(1024)
             plaintext = server_cipherstates[0].decrypt_with_ad(b'', ciphertext)
