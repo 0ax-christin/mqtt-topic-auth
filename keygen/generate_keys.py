c5f900c9405eaad3f50d2f895226dcb9ca040edb
Christin <caa8062@rit.edu>
2024-02-25 15:21:10 +0400

    Renamed generate-keys to generate_keys, modified the generate keypair function to optionally give bytes version of keypair if specified in a parameter

--- keygen/generate_keys.py
+++ keygen/generate_keys.py
@@ -2,9 +2,9 @@ from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from cryptography.hazmat.primitives import serialization
 import os
 
-def generate_ED25519_keypair():
-    if os.path.exists('private_key.txt'):
-        with open('private_key.txt', 'rb') as reader:
+def generate_ED25519_keypair(isBytes: bool = False):
+    if os.path.exists('../keys/client/private_key.txt'):
+        with open('../keys/client/private_key.txt', 'rb') as reader:
             private_bytes = reader.read()
         private_key = Ed25519PrivateKey.from_private_bytes(private_bytes)
     else:    
@@ -14,15 +14,18 @@ def generate_ED25519_keypair():
             format=serialization.PrivateFormat.Raw,
             encryption_algorithm=serialization.NoEncryption()
         )
-        with open('private_key.txt', 'wb') as writer:
+        with open('../keys/client/private_key.txt', 'wb') as writer:
             writer.write(private_bytes)
         public_bytes = private_key.public_key().public_bytes(
             encoding=serialization.Encoding.Raw,
             format=serialization.PublicFormat.Raw
         )
-        with open('public_key.txt', 'wb') as writer:
+        with open('../keys/client/public_key.txt', 'wb') as writer:
             writer.write(public_bytes)
-    return (private_key, private_key.public_key())
+    if isBytes:
+        return (private_bytes, public_bytes)
+    else:
+        return (private_key, private_key.public_key())
 
 def main():
     private_key, public_key = generate_ED25519_keypair()
