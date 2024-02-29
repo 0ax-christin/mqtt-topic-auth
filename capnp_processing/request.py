4f6b60c28b0a7e28cbdf5cd0af26564f6e809eaa
Christin <caa8062@rit.edu>
2024-02-29 15:55:32 +0400

    Modified generate_request to return the bytes version of the capnproto request and optionally, if a cipherstate of noise is given, it encrypts the bytes and returns that back

--- capnp_processing/request.py
+++ capnp_processing/request.py
@@ -3,11 +3,14 @@ import capnp
 capnp.remove_import_hook()
 request_capnp = capnp.load('capnp_schemas/request.capnp')
 
+from dissononce.processing.impl.cipherstate import CipherState
+
 request = request_capnp.Request
 
 # Generate a request object as specified and return as bytes
 # For network transmission
-def generate_request(requestType: str, nonce: bytes = None, solution: bool = False, status: bool = False, statusCode: int = None) -> request_capnp.Request:
+# Optionally, if cipherstate is included, the function will encrypt with this cipherstate with no associated data and return the bytes
+def generate_request_bytes(requestType: str, cipherState: CipherState = None, assoc_data: bytes = b'', nonce: bytes = None, solution: bool = False, status: bool = False, statusCode: int = None) -> bytes:
     allowed_request_types = ['register', 'challenge', 'response', 'reauth', 'status']
 
     if requestType in allowed_request_types:
@@ -21,6 +24,11 @@ def generate_request(requestType: str, nonce: bytes = None, solution: bool = Fal
                 new_request.nonceSolution = nonce
             else:
                 new_request.nonceChallenge = nonce
-            
+
+    new_request_bytes = new_request.to_bytes_packed()
+
+    if cipherState != None:
+        enc_new_req_bytes = cipherState.encrypt_with_ad(assoc_data, new_request_bytes)
+        return enc_new_req_bytes
     # else raise an Exception
-    return new_request
+    return new_request_bytes
