81ecc9848291595db325f7ffd863a828e25df4bb
Christin <caa8062@rit.edu>
2024-02-29 11:40:25 +0400

    Added the used for transport of messages in MQTT using public and subscribe. Given a noise cipherstate, all the payloads are encrypted before being published, ensuring confidentiality

--- mqtt/mqtt_pub_sub.py
+++ mqtt/mqtt_pub_sub.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MQTT message payloads are always transferred in bytes
-async def publish(server_cipherstate: CipherState, mqtt_username: str, mqtt_password: str, mqtt_topic: str, mqtt_payload: bytes):
+async def publish(cipherstate: CipherState, mqtt_username: str, mqtt_password: str, mqtt_topic: str, mqtt_payload: bytes):
     #1.Encrypt whatever payload with noise cipherstate
     enc_payload_bytes = server_cipherstate.encrypt_with_ad(b'', mqtt_payload)
     # 2. Transmit in the payload of a MQTT publish message to auth topic
     async with aiomqtt.Client(hostname=os.getenv("HOSTNAME"), port=os.getenv("PORT"), username=mqtt_username, password=mqtt_password, protocol=os.getenv("PROTOCOL_VER")) as client:
         await client.publish(mqtt_topic, payload=enc_payload_bytes)
 
-async def subscribe(server_cipherstate: CipherState, mqtt_username: str, mqtt_password: str, mqtt_topic: str) -> bytes:
+async def subscribe(cipherstate: CipherState, mqtt_username: str, mqtt_password: str, mqtt_topic: str) -> bytes:
     async with aiomqtt.Client(hostname=os.getenv("HOSTNAME"), port=os.getenv("PORT"), username=mqtt_username, password=mqtt_password, protocol=os.getenv("PROTOCOL_VER")) as client:
         await client.subscribe(mqtt_topic)
         # We expect only one message from the server, which is returned
