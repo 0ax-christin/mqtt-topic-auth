8cbd47b6ed95d6bf799bdee6464a57cfd4d2c2a2
Christin <caa8062@rit.edu>
2024-02-29 15:56:37 +0400

    Added functions for the generation and verification of HMAC responses for the challenge solving phase

--- authentication/challenges.py
+++ authentication/challenges.py
@@ -1,20 +1,24 @@
 import secrets, socket
 
 from capnp_processing.request import generate_request
+from cryptography.hazmat.primitives import hashes, hmac
 
-def generate_challenge() -> int:
-    """ Generates a random integer of 64 bits for use as a challenge
+# Given a challenge of bytes
+# and a HMAC key, generate an HMAC(key, challenge) and return
+def generate_challenge_response(nonce_challenge: bytes, hmac_key: bytes) -> bytes:
+    h = hmac.HMAC(key, hashes.BLAKE2s(32))
+    h.update(nonce_challenge)
+    response = h.finalize()
+    return response 
 
-    Returns
-    -------
-    int
-        Random integer that will be used as a challenge    
-    """
-    random_bits = secrets.randbits(64)
-    return random_bits
-
-
-def send_challenge(socket: socket.socket, challenge: int):
-    challenge = generate_request(requestType='challenge',  nonce=challenge.to_bytes(64), solution=False)
-    # Send challenge in bytes
-    socket.sendall(challenge.to_bytes_packed())
+# Given the original nonce challenge and hmac key, verify that the supplied nonce solution is valid
+# If so, return true, else false
+def verify_challenge_response(nonce_challenge: bytes, nonce_solution: bytes, hmac_key: bytes) -> bool:
+    h = hmac.HMAC(key, hashes.BLAKE2s(32))
+    h.update(challenge.to_bytes(64))
+    try:
+        h.verify(response_request.nonceSolution)
+        print("Verified!!!!")
+        return True
+    except InvalidSignature:
+        return False
