5602a1bde8a2678ce3ef46159d35c45a6a3e7b9f
Christin <caa8062@rit.edu>
2024-04-07 16:57:46 +0400

    Modified functions to store keys using Hashicorp vault

--- keygen/generate_keys.py
+++ keygen/generate_keys.py
@@ -1,44 +1,80 @@
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from cryptography.hazmat.primitives import serialization
-import os
+import os, pickle, hvac
+
+from hvac.exceptions import InvalidPath
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from pathlib import Path
 
 # Base path of the project directory
 base_path = Path(__file__).parent
 
-def generate_ED25519_keypair(isBytes: bool = False, server: bool = False):
-    if server:
-        entity = 'server'
-    else:
-        entity = 'client'
-    if os.path.exists((base_path / f'../keys/{entity}/private_key.txt').resolve()):
-        with open((base_path / f'../keys/{entity}/private_key.txt').resolve(), 'rb') as reader:
-            private_bytes = reader.read()
-        private_key = Ed25519PrivateKey.from_private_bytes(private_bytes)
-        public_bytes = private_key.public_key().public_bytes(
-            encoding=serialization.Encoding.Raw,
-            format=serialization.PublicFormat.Raw
-        )
-    else:    
+def generate_ED25519_keypair_server_bytes(pickled_server_static):
+    client = hvac.Client(url=os.getenv("VAULT_HOST"), token=os.getenv("VAULT_TOKEN"))
+    response = client.secrets.kv.read_secret_version("server")
+    server_identity = response['data']['data']['identity_keypair']
+    if server_identity == '':
+        print("Server identity keypair not present, generating...")
         private_key = Ed25519PrivateKey.generate()
-        private_bytes = private_key.private_bytes(
+        server_identity_bytes = private_key.private_bytes(
             encoding=serialization.Encoding.Raw,
             format=serialization.PrivateFormat.Raw,
             encryption_algorithm=serialization.NoEncryption()
         )
-        with open((base_path / f'../keys/{entity}/private_key.txt').resolve(), 'wb') as writer:
-            writer.write(private_bytes)
-        public_bytes = private_key.public_key().public_bytes(
+        print("Pickled Server static: ", pickled_server_static)
+        print("Identity keypair hex:", server_identity_bytes.hex())
+        client.secrets.kv.v2.create_or_update_secret(
+            path='server',
+            secret=dict(noise_static_keypair=pickled_server_static, identity_keypair=server_identity_bytes.hex())
+        ) 
+    else:
+        print("Server identity has a value in the Vault, loading this..")
+        hexed_server_identity = client.secrets.kv.read_secret_version(path="server")['data']['data']['identity_keypair']
+        print("Hexed server identity", hexed_server_identity)
+        private_key = Ed25519PrivateKey.from_private_bytes(bytes.fromhex(hexed_server_identity))
+    
+    private_bytes = private_key.private_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PrivateFormat.Raw,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+
+    public_bytes = private_key.public_key().public_bytes(
             encoding=serialization.Encoding.Raw,
             format=serialization.PublicFormat.Raw
-        )
-        with open((base_path / f'../keys/{entity}/public_key.txt').resolve(), 'wb') as writer:
-            writer.write(public_bytes)
-    if isBytes:
-        return (private_bytes, public_bytes)
-    else:
-        return (private_key, private_key.public_key())
+    )
+    return (private_bytes, public_bytes)
+
+def generate_ED25519_keypair_client_bytes():
+    if os.path.exists((base_path / f'../keys/client/private_key.txt').resolve()):
+        with open((base_path / f'../keys/client/private_key.txt').resolve(), 'rb') as reader:
+            private_bytes = reader.read()
+        private_key = Ed25519PrivateKey.from_private_bytes(private_bytes)
+    else:    
+        private_key = Ed25519PrivateKey.generate()
+        with open((base_path / f'../keys/client/private_key.txt').resolve(), 'wb') as writer:
+            writer.write(private_key.private_bytes(
+                encoding=serialization.Encoding.Raw,
+                format=serialization.PrivateFormat.Raw,
+                encryption_algorithm=serialization.NoEncryption()))
+        with open((base_path / f'../keys/client/public_key.txt').resolve(), 'wb') as writer:
+            writer.write(private_key.public_key().public_bytes(
+                encoding=serialization.Encoding.Raw,
+                format=serialization.PublicFormat.Raw))
+    
+    private_bytes = private_key.private_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PrivateFormat.Raw,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    public_bytes = private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )
+    return (private_bytes, public_bytes)
 
 def main():
     private_key, public_key = generate_ED25519_keypair()
